a0fc52e84b9e587dc5f3e18ff75b6778504418d8
Danniely Santos <dannielyingrid@gmail.com>
2020-08-12 17:49:30 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
-## code your solution here. 
+class Cat
+  maru = Cat.new
+  maru.name = "Maru"
+  maru.name 
+end
+
+class Cat 
+  attr_accessor :cat
+end 
+
+maru.name 
+  
+  def meow
+    puts "meow!"
+  end
+end 
