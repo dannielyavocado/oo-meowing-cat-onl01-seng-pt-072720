c826610111135199312db9ba9f2ae42975e37934
Danniely Santos <dannielyingrid@gmail.com>
2020-08-12 17:50:05 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,15 +1,7 @@
-class Cat
-  maru = Cat.new
-  maru.name = "Maru"
-  maru.name 
-end
-
 class Cat 
-  attr_accessor :cat
+  attr_accessor :name
 end 
 
-maru.name 
-  
   def meow
     puts "meow!"
   end
